f68363bb8e05fd8ccd16f3cfa64393bc26fc660e
YoungsonMan <choi13570@naver.com>
2024-08-23 20:01:03 +0900

    [Feat] 프로펠러 회전 추가
    프로펠러가 회전 기능을 추가하였습니다.

--- Assets/Scenes/HeliFunctions/propellerRotate.cs
+++ Assets/Scenes/HeliFunctions/propellerRotate.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class propellerRotate : MonoBehaviour
 {
     public int flyLimit;
     public Rigidbody rigid;
 
+    [SerializeField] float angle;
+    [SerializeField] float revolutionPower;
+
+
+    public Transform target;
 
     private void Update()
     {
+       RotateAround();
        if ( Input.GetButtonDown("Fly"))
         {
-            Debug.Log("_____ is down");
+            Debug.Log("Fly Button is down");
         }
         if (Input.GetButton("Fly"))
         {
-            Debug.Log("_____ is pressing");
+            Debug.Log("Fly Button is pressing");
         }
         if (Input.GetButtonUp("Fly"))
         {
-            Debug.Log("_____ is up");
+            Debug.Log("Fly Button is up");
         }
+    }
+    private void RotateAround() // 기준점을 중심으로 회전
+    {
+        transform.RotateAround(target.position, Vector3.up, revolutionPower * Time.deltaTime);
     }
 
 }
